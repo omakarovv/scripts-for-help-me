d0ef28dfa72d440a450d53dddafb8264dc5f2daf
omakarovv <makarov.oleg@gmail.com>
2017-09-13 11:19:14 +0300

    Create find-files.py

---
+++ find_old_files.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import re
+import datetime
+
+
+# Directory for search.
+dir = '/'
+
+# REGEX for exclude path: dev,sys,proc.
+pattern = re.compile('^\/[dev,proc,sys]\/*')
+
+# Date and time now.
+dnow = datetime.datetime.now()
+
+# Begin search and create list files.
+for root, dirs, files in os.walk(dir):
+    for name in files:
+
+        """
+        - dirname  -> Get directory name
+        - fullname -> fullpath to the file
+        - filepath -> check existing file or directory
+        """
+
+        dirname = os.path.join(root)
+        fullname = os.path.join(root, name)
+
+#If dirname is pattern match do nothing.
+        if pattern.match(dirname):
+            continue
+
+# Else check file exist and file size.
+        else:
+            if os.path.exists(fullname) and os.path.isfile(fullname):
+                sz = os.path.getsize(fullname) >> 20L
+
+                dmodify = (
+                   datetime.datetime.fromtimestamp(os.path.getmtime(fullname))
+                  )
+
+                days_diff = (dnow-dmodify).days
+
+                if sz > 100 and days_diff <= 30:
+                    print '%s |Size is: %sMb | Last Modified: %s Days ago' % (
+                          (fullname, sz, days_diff)
+                         )
