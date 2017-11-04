aa026dc61985b3c6b5fd3c306cdc2bb9b56eaa63
omakarovv <makarov.oleg@gmail.com>
2017-11-05 00:44:25 +0300

    Update OnlineLogReader_v2.py

--- FlaskProject/OnlineLogReader_v2.py
+++ FlaskProject/OnlineLogReader_v2.py
@@ -5,17 +5,19 @@ import time
 app = Flask(__name__)
 
 def collect_rows():
-    while True:
-        with open('test_log.log', 'r') as f:
-            for line in f:
+    with open('test_log.log', 'r') as f:
+        while 1:
+            where = f.tell()
+            line = f.readline()
+            if not line:
+                f.seek(where)
+            else:
                 yield "".join(line) + '\n'
 
 @app.route('/')
 
 def present_log():
     return Response(collect_rows(), mimetype='text')
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
