6cf7f7989eeebb162a9eb19a7feeb694c07094bb
omakarovv <makarov.oleg@gmail.com>
2017-11-05 01:53:34 +0300

    Update ReadFile_online_FromConsole.py

--- FlaskProject/ReadFile_online_FromConsole.py
+++ FlaskProject/ReadFile_online_FromConsole.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 import time
+import os
 
 with open('test_log.log', 'r') as f:
     while 1:
-        position = f.tell()
+        where = f.tell()
+
+        if os.stat('test_log.log').st_size == 0:
+            where = 0
+
         line = f.readline()
         if not line:
             time.sleep(1)
-            f.seek(position)
+            f.seek(where)
         else:
             print(line)
