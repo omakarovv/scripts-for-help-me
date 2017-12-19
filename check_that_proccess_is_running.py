dbc37c5deb59c5ec5caae683cf55604c91f61318
omakarovv <makarov.oleg@gmail.com>
2017-12-20 01:00:17 +0300

    Create check_that_proccess_is_running.py

---
+++ check_that_proccess_is_running.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+
+import os
+import time
+
+
+pid = os.getpid()
+pid_file = "pid_file"
+
+def is_proccess_running():
+    if not os.path.exists(pid_file):
+        return True
+    else:
+       old_pid_file = open(pid_file, 'r')
+       old_pid = int(old_pid_file.readline())
+       try:
+           check_old_pid = os.kill(old_pid, 0)
+       except:
+           return True
+           
+if is_proccess_running():
+    with open(pid_file, 'w') as f:
+        f.write(str(pid))
+
+    try:
+        x = 30
+        while x > 0:
+            time.sleep(5)
+            x -= 1
+            print(pid)
+    except:
+        raise
+    finally:
+        os.remove(pid_file)
